1beb271ab050960919e4ecf77e9b0aded72feec3
Your Name <markhallak@outlook.com>
2021-07-07 14:31:23 +0300

    Initial Commit

--- app/src/main/java/com/example/ourclimate/MainActivity.java
+++ app/src/main/java/com/example/ourclimate/MainActivity.java
@@ -10,5 +10,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+        initListeners();
+    }
+
+    // initialize the views here
+    private void initViews() {
+    }
+
+    // add the listeners here
+    private void initListeners() {
     }
 }
